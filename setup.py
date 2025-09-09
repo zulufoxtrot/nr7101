7a697e4ca08fa99089316305f3aba8547edd957f
zulufoxtrot <youri.lcd@gmail.com>
2025-09-09 14:52:10 +0200

    v2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nr7101",
-    version="2.0.0",
+    version="2.0.2",
     description="Zyxel NR7101 tool",
     author="Pekka Korpinen",
     author_email="pekka.korpinen@iki.fi",
